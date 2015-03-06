8e78180feee90beb465dee4ca0f880e0fc272636
Euan T <euantorano@gmail.com>
2015-03-06 10:59:15 +0000

    Added facade for settings
    Signed-off-by: Euan T <euantorano@gmail.com>

---
+++ src/Facade.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Settings facade.
+ *
+ * @author MyBB Group
+ * @version 2.0.0
+ * @package mybb/settings
+ * @copyright Copyright (c) 2014, MyBB Group
+ * @license http://www.mybb.com/about/license GNU LESSER GENERAL PUBLIC LICENSE
+ * @link http://www.mybb.com
+ */
+
+namespace MyBB\Settings;
+
+class Facade extends \Illuminate\Support\Facades\Facade
+{
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 *
+	 * @throws \RuntimeException
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'MyBB\Settings\Manager';
+	}
+}
