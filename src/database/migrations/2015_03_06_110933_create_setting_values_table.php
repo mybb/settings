b1b67f01e130292db8b16e2647733aa9d034c320
Josh Harmon <josh@joshharmon.me>
2016-07-04 12:13:22 -0700

    Convert tabs to spaces in migrations

--- src/database/migrations/2015_03_06_110933_create_setting_values_table.php
+++ src/database/migrations/2015_03_06_110933_create_setting_values_table.php
@@ -16,31 +16,31 @@ use Illuminate\Database\Schema\Blueprint;
 class CreateSettingValuesTable extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('setting_values', function (Blueprint $table) {
-			$table->increments('id');
-			$table->unsignedInteger('setting_id');
-			$table->string('value');
-			$table->unsignedInteger('user_id')->nullable();
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('setting_values', function (Blueprint $table) {
+      $table->increments('id');
+      $table->unsignedInteger('setting_id');
+      $table->string('value');
+      $table->unsignedInteger('user_id')->nullable();
 
-			$table->foreign('setting_id')->references('id')->on('settings')->onDelete('cascade');
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-		});
-	}
+      $table->foreign('setting_id')->references('id')->on('settings')->onDelete('cascade');
+      $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('setting_values');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('setting_values');
+  }
 }
--- src/database/migrations/2015_03_07_220139_remove_is_user_setting_from_settings_table.php
+++ src/database/migrations/2015_03_07_220139_remove_is_user_setting_from_settings_table.php
@@ -16,37 +16,37 @@ use Illuminate\Database\Schema\Blueprint;
 class RemoveIsUserSettingFromSettingsTable extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('settings', function (Blueprint $table) {
-			$table->dropColumn('is_user_setting');
-		});
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('settings', function (Blueprint $table) {
+      $table->dropColumn('is_user_setting');
+    });
 
-		Schema::table('setting_values', function (Blueprint $table) {
-			$table->unique(['setting_id', 'user_id'], 'setting_values_setting_id_user_id_unique');
-		});
-	}
+    Schema::table('setting_values', function (Blueprint $table) {
+      $table->unique(['setting_id', 'user_id'], 'setting_values_setting_id_user_id_unique');
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('settings', function (Blueprint $table) {
-			$table->boolean('is_user_setting')->default(false);
-		});
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('settings', function (Blueprint $table) {
+      $table->boolean('is_user_setting')->default(false);
+    });
 
-		Schema::table('setting_values', function (Blueprint $table) {
-			$table->dropForeign('setting_values_setting_id_foreign');
-			$table->dropUnique('setting_values_setting_id_user_id_unique');
-			$table->foreign('setting_id')->references('id')->on('settings')->onDelete('cascade');
-		});
-	}
+    Schema::table('setting_values', function (Blueprint $table) {
+      $table->dropForeign('setting_values_setting_id_foreign');
+      $table->dropUnique('setting_values_setting_id_user_id_unique');
+      $table->foreign('setting_id')->references('id')->on('settings')->onDelete('cascade');
+    });
+  }
 }
